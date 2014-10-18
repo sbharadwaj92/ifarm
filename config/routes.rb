2308461dd42b35f055300638d6b5730be1cfb5a6
Santosh Bharadwaj <sbharadwaj92@yahoo.co.in>
2014-10-19 01:50:31 +0530

    made lot of changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  get "about" => "pages#about" # Creats about_path
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
