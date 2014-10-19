0e8ab03c9345c0b10ffa11dd2ec8bf35641c2a05
Santosh Bharadwaj <sbharadwaj92@yahoo.co.in>
2014-10-20 01:54:57 +0530

    fixed the columns for user resource in activeadmin

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,5 +1,16 @@
 ActiveAdmin.register User do
   #remove_filter :storage_total_stat
+  index do
+    selectable_column
+    id_column
+    column :name
+    column :email
+    column :sign_in_count
+    column :created_at
+    column :confirmed_at
+    actions
+  end
+
   form do |f|
     f.inputs do
       f.input :name
@@ -9,6 +20,8 @@ ActiveAdmin.register User do
     end
     f.actions
   end
+  filter :name
+  filter :email
   permit_params :name, :email, :password, :password_confirmation
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
