b9789afbf0b8a54189615e099e4eccfc009a6331
Santosh Bharadwaj <sbharadwaj92@yahoo.co.in>
2014-10-20 15:00:23 +0530

    made few changes for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -86,7 +86,7 @@ Rails.application.configure do
     }
   }
 
-  config.action_mailer.default_url_options = { :host => 'example.com' }
+  config.action_mailer.default_url_options = { :host => 'san-survey.herokuapp.com' }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
@@ -96,15 +96,15 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
   address: "smtp.gmail.com",
   port: 587,
-  domain: ENV["GMAIL_DOMAIN"],
+  domain: "gmail.com",
   authentication: "plain",
   enable_starttls_auto: true,
-  user_name: ENV["GMAIL_USERNAME"],
-  password: ENV["GMAIL_PASSWORD"]
+  user_name: "noreply.sant",
+  password: "meghana123"
   }
 
 
-  Rails.application.routes.default_url_options[:host] = 'example.com'
+  Rails.application.routes.default_url_options[:host] = 'san-survey.herokuapp.com'
 
 
 end
