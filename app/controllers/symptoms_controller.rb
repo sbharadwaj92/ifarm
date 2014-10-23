fbcce063bce75e3258933253353713ee363472fb
Santosh Bharadwaj <sbharadwaj92@yahoo.co.in>
2014-10-23 21:27:09 +0530

    changed lot of things

--- app/controllers/symptoms_controller.rb
+++ app/controllers/symptoms_controller.rb
@@ -1,2 +1,12 @@
 class SymptomsController < InheritedResources::Base
+  def create
+    @crop = Crop.find(params[:crop_id])
+    @symptom = @crop.symptoms.create(symptom_params)
+    redirect_to crop_path(@crop)
+  end
+ 
+  private
+    def symptom_params
+      params.require(:symptom).permit(:title, :text, :image)
+    end
 end
--- app/models/symptom.rb
+++ app/models/symptom.rb
@@ -1,3 +1,7 @@
 class Symptom < ActiveRecord::Base
   belongs_to :crop
+  validates :title, presence: true,
+                    length: { minimum: 5 }
+  has_attached_file :image, styles: { small: "64x64", med: "100x100", large: "200x200" }
+  validates_attachment :image, :presence => true, :content_type => { :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
 end
