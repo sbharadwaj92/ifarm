3a9098a4cd57dcae3dbbe499e18f3aff3a3cd8bf
Santosh Bharadwaj <sbharadwaj92@yahoo.co.in>
2014-10-29 22:49:54 +0530

    fixed views for both crops and symptoms

--- app/controllers/symptoms_controller.rb
+++ app/controllers/symptoms_controller.rb
@@ -1,10 +1,41 @@
 class SymptomsController < InheritedResources::Base
+  
+  def new
+    @crop = Crop.find(params[:crop_id])
+    @symptom = @crop.symptoms.build
+    respond_with(@symptom)
+  end
+
+  def index
+    @crop = Crop.find(params[:crop_id])
+    @symptom = Symptom.find_by_id(params[:all])
+    @symptoms = Symptom.all.order("created_at ASC").paginate(:page => params[:page], :per_page => 2)
+  end
+
+  def show
+    @crop = Crop.find(params[:crop_id])
+    @symptom = Symptom.find(params[:id])
+  end
+
+  def edit
+    @crop = Crop.find(params[:crop_id])
+    @symptom = Symptom.find(params[:id])
+  end
+
   def create
     @crop = Crop.find(params[:crop_id])
     @symptom = @crop.symptoms.create(symptom_params)
     redirect_to crop_path(@crop)
   end
- 
+
+  def destroy
+    @symptom = Symptom.find(params[:id])
+    @symptom = @crop.symptom.find(params[:id])
+    @symptom.destroy
+    redirect_to symptoms_path
+
+  end
+
   private
     def symptom_params
       params.require(:symptom).permit(:title, :text, :image)
