2462cc6e0915de8faf487a2d5203ada9a3282d6b
Santosh Bharadwaj <sbharadwaj92@yahoo.co.in>
2014-10-29 13:37:18 +0530

    lot of changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,12 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   validates :name, presence: true
-  validates :username, uniqueness: true
+  validates :username, presence: true, uniqueness: true
   
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable
+
+  validates_presence_of    :password, on: :create
+  validates_confirmation_of :password, on: :create
 
 end
